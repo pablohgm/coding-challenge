12e1f4f9cbff160a4f126abc0c88e372387940a2
Pablo Gonzalez <pablohgm@gmail.com>
2022-07-10 22:41:04 -0600

    feat(sorted-merge): add implementation for async and sync sorted merge

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -2,7 +2,36 @@
 
 // Print all entries, across all of the *async* sources, in chronological order.
 
-module.exports = (logSources, printer) => {
+const _ = require("lodash");
+const P = require("bluebird");
+
+module.exports = async (logSources, printer) => {
+  // Fill out the map with the first log of each log source using promise all
+  const swapMap = new Map()
+  const responses = await P.all(logSources.map((sourceLog) => sourceLog.popAsync()))
+  _.each(responses, (log, index) => {
+    if (log) {
+      swapMap.set(index, log)
+    }
+  })
+
+  for (const [,] of swapMap) {
+    // Find the oldest log
+    const [oldestKey, oldestLog] = _.minBy([...swapMap.entries()], ([, log]) => log.date)
+
+    // Print and delete log from map
+    printer.print(oldestLog)
+    swapMap.delete(oldestKey)
+
+    // refilled log from log source
+    const refilledLog = await logSources[oldestKey].popAsync()
+    if (refilledLog) {
+      swapMap.set(oldestKey, refilledLog)
+    }
+  }
+
+  printer.done()
+
   return new Promise((resolve, reject) => {
     resolve(console.log("Async sort complete."));
   });
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,35 @@
 "use strict";
 
+const _ = require("lodash");
+
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
+  // Fill out the map with the first log of each log source
+  const swapMap = new Map()
+  _.each(logSources, (sourceLog, index) => {
+    const log = sourceLog.pop()
+    if (log) {
+      swapMap.set(index, log)
+    }
+  })
+
+  swapMap.forEach((key, value, map) => {
+    // Find the oldest log
+    const [oldestKey, oldestLog] = _.minBy([...map.entries()], ([, log]) => log.date)
+
+    // Print and delete log from map
+    printer.print(oldestLog)
+    map.delete(oldestKey)
+
+    // refilled log from log source
+    const refilledLog = logSources[oldestKey].pop()
+    if (refilledLog) {
+      map.set(oldestKey, refilledLog)
+    }
+  })
+
+  printer.done()
+
   return console.log("Sync sort complete.");
 };
